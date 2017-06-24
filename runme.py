f73910c20af254cd99e854a486e797cbef49908e
easyfly007 <easyfly.huang@gmail.com>
2017-06-24 15:52:45 +0800

    code bug fix

--- runme.py
+++ runme.py
@@ -18,8 +18,9 @@ for i in range(num_episodes):
 	j = 0
 	while j < 99:
 		j += 1
-		a = np.argmax(Q[s, :] + np.random.randn(1, env.action_space.n)*(1.0/(i+1)))
+		a = np.argmax(Q[s, :] + np.random.randn(1, env.action_space.n)*(1.0/(i+1.0)))
 		s1, r, d, _ = env.step(a)
+		Q[s,a] = Q[s, a] + lr*(r + y*np.max(Q[s1,:]) - Q[s, a])
 		rAll += r 
 		s = s1
 		if d == True:
